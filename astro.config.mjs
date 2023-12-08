6280ca1d2656aa621e835d48eff035093f810951
flawedhuman <110935159+flawedhuman@users.noreply.github.com>
2023-12-08 13:22:57 -0600

    fixing image/page urls

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,4 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-    site: 'https://flawedhuman.github.io',
-    base: '/Terrible-Geralds-V1/',
-});
+export default defineConfig({});
