e8bcfc5baf4f5c098b65bc6595a024ded7fe593c
Matt Goddard <110935159+flawedhuman@users.noreply.github.com>
2023-12-24 05:34:21 -0600

    adds copyright, styles dialog

--- src/components/vue/form.vue
+++ src/components/vue/form.vue
@@ -1,35 +1,42 @@
 <template>
-    <div v-if="!isFormSubmitted" class="items">
-        <div>
-            <label for="name">Name</label>
-            <input type="text" name="name" id="name" v-model="name" />
+    <dialog>
+        <div class="close-contact-form-wrap">
+            <button class="close-contact-form">X</button>
         </div>
-        <div>
-            <label for="emailPhone">Email or Phone Number</label>
-            <input type="text" name="emailPhone" id="emailPhone" v-model="emailPhone" />
-        </div>
-        <div>
-            <label for="date">Event Date</label>
-            <input type="date" name="date" id="date" v-model="date" />
-        </div>
-        <div>
-            <label for="location">Event Location</label>
-            <input type="text" name="location" id="location" v-model="location" />
-        </div>
-        <div>
-            <label for="count">Estimated Guest Count</label>
-            <input type="text" name="count" id="count" v-model="count" />
-        </div>
-        <div>
-            <label for="message">Additional Info</label>
-            <textarea id="message" name="message" rows="6" v-model="message"></textarea>
-        </div>
-        <span class="error">{{error}}</span>
-        <button class="btn btn-primary" @click="submit">Submit</button>
-    </div>
-    <div v-if="isFormSubmitted">
-        <h1>Thank you for submitting the form</h1>
-    </div>
+        <form method="dialog">
+            <div v-if="!isFormSubmitted" class="items">
+                <div>
+                    <label for="name">Name</label>
+                    <input type="text" name="name" id="name" v-model="name" />
+                </div>
+                <div>
+                    <label for="emailPhone">Email or Phone Number</label>
+                    <input type="text" name="emailPhone" id="emailPhone" v-model="emailPhone" />
+                </div>
+                <div>
+                    <label for="date">Event Date</label>
+                    <input type="date" name="date" id="date" v-model="date" />
+                </div>
+                <div>
+                    <label for="location">Event Location</label>
+                    <input type="text" name="location" id="location" v-model="location" />
+                </div>
+                <div>
+                    <label for="count">Estimated Guest Count</label>
+                    <input type="text" name="count" id="count" v-model="count" />
+                </div>
+                <div>
+                    <label for="message">Additional Info</label>
+                    <textarea id="message" name="message" rows="6" v-model="message"></textarea>
+                </div>
+                <span class="error">{{error}}</span>
+                <button class="btn btn-secondary" @click.prevent="submit">Submit</button>
+            </div>
+            <div v-if="isFormSubmitted">
+                <h1>Thank you for submitting the form</h1>
+            </div>
+        </form>
+    </dialog>
 </template>
 
 <script>
@@ -97,6 +104,36 @@ export default {
 
     }
 
+    .close-contact-form-wrap {
+        width: 100%;
+        text-align: right;
+    }
+
+    @media (max-width: 700px) {
+
+        .close-contact-form-wrap {
+            max-width: 550px;
+            width: 90%;
+            text-align: right;
+        }
+
+    }
+
+    #full-screen-wrap button.close-contact-form {
+        font-size: 3rem;
+        text-align: right;
+        background-color: transparent;
+        border: none;
+        font-family: 'Bobby Jones Soft Regular', 'League Spartan', Arial, Helvetica, sans-serif;
+        cursor: pointer;
+        color: rgba(245, 9, 36, 1);
+    }
+
+    #full-screen-wrap button.close-contact-form:is(:hover, :focus) {
+        color: #000;
+        rotate: 5deg;
+    }
+
     .items label {
         margin-bottom: 0.5rem;
         display: block;
@@ -126,6 +163,10 @@ export default {
         border-bottom: 3px #000 solid;
         rotate: 1deg;
         font-family: 'Bobby Jones Soft Regular', 'League Spartan', Arial, Helvetica, sans-serif;
+    }
+
+    dialog::backdrop {
+        background-color: rgba(0, 0, 0, 0.9);
     }
 
 </style>
